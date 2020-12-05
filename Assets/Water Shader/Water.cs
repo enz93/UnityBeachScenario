63ef0875ca35d5239549226b9d5037bd621ad94d
Vincenzo <terracciano.vincenzo.1993@gmail.com>
2020-12-05 20:22:33 +0100

    ultimate release

---
+++ Assets/Water Shader/Water.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+[ExecuteInEditMode] // For DepthTextureMode
+
+
+public class Water : MonoBehaviour
+{
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+       
+
+    public float UvRotateSpeed = 0.4f;
+    public float UvRotateDistance = 2.0f;
+    public float UvBumpRotateSpeed = 0.4f;
+    public float UvBumpRotateDistance = 2.0f;
+    public bool depthTextureModeOn = true;
+
+    //---------------------------------
+
+    Vector2 lwVector, lwNVector;
+    
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    private void Awake()
+    {
+        //---------------------------------
+
+        lwVector = Vector2.zero;
+        lwNVector = Vector2.zero;
+
+        //---------------------------------
+    }
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    void OnEnable()
+    {
+        //---------------------------------
+
+        if (depthTextureModeOn) Camera.main.depthTextureMode = DepthTextureMode.Depth;
+
+        //---------------------------------
+    }
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    void Update()
+    {
+        //---------------------------------
+
+        lwVector = Quaternion.AngleAxis(Time.time * UvRotateSpeed, Vector3.forward) * Vector2.one * UvRotateDistance;
+        lwNVector = Quaternion.AngleAxis(Time.time * UvBumpRotateSpeed, Vector3.forward) * Vector2.one * UvBumpRotateDistance;
+
+        //---------------------------------
+
+        Shader.SetGlobalFloat("_WaterLocalUvX", lwVector.x);
+        Shader.SetGlobalFloat("_WaterLocalUvZ", lwVector.y);
+        Shader.SetGlobalFloat("_WaterLocalUvNX", lwNVector.x);
+        Shader.SetGlobalFloat("_WaterLocalUvNZ", lwNVector.y);
+
+        //---------------------------------
+    }
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}
